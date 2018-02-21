f49e7e38d60a3687e2fffc8ba903cff87e49dfe8
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-21 18:21:57 -0500

    Notes and a new part of $scope

--- StandaloneComponents/js/directives/appInfo.js
+++ StandaloneComponents/js/directives/appInfo.js
@@ -1,10 +1,12 @@
 app.directive('appInfo', function () {
 	return {
+		// restrict specifies how this will be used. 'E' means an HTML element
 		restrict: 'E',
+		// pass information through an attribute named info. 
 		scope: {
 			info: '='
 		},
+		// where we get the template (HTML in this case) for scope
 		templateUrl: 'js/directives/appInfo.html'
 	};
-})
-;
+});
