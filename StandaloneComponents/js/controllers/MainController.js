47b2aa410e86cd3533310e260f9a4b81deef2550
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-23 10:31:23 -0500

    New directives and whatnot

--- StandaloneComponents/js/controllers/MainController.js
+++ StandaloneComponents/js/controllers/MainController.js
@@ -19,4 +19,31 @@ app.controller('MainController', ['$scope', function ($scope) {
 		developer: 'Armando P.',
 		price: 1.99
 	};
+
+	$scope.apps = [
+		{
+			icon: 'icon/move.jpg',
+			title: 'MOVE',
+			developer: 'MOVE, Inc.',
+			price: 0.99
+		},
+		{
+			icon: 'img/shutterbugg.jpg',
+			title: 'Shutterbugg',
+			developer: 'Chico Dusty',
+			price: 2.99
+		},
+		{
+			icon: 'img/move.jpg',
+			title: 'Twitter',
+			developer: 'Twitter, Inc.',
+			price: 0
+		},
+		{
+			icon: 'img/move.jpb',
+			title: 'Apple Maps is good and definitely doesn\'t suck',
+			developer: 'Smart people',
+			price: 0	
+		}
+	];
 }]);
