ef7e782432e7096206f36c17bd18e853aecd7616
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-24 09:12:06 -0500

    Fixed routing error

--- Routing/js/app.js
+++ Routing/js/app.js
@@ -1,13 +1,14 @@
 var app = angular.module('GalleryApp', ['ngRoute']);
 
-// Routing information
 app.config(function ($routeProvider) {
-  $routeProvider.when('/', {
-    'controller': 'HomeController',
-    'templateUrl': 'views/home.html'
-  }).when('/photos/:id', {
-    'controller': 'PhotoController',
-    'templateUrl': 'views/photo.html'
-  })
-.otherwise({'redirectTo': '/'});
+  $routeProvider
+    .when('/', {
+      'controller': 'HomeController',
+      'templateUrl': 'views/home.html'
+    })
+    .when('/photos/:id', {
+      'controller': 'PhotoController',
+      'templateUrl': 'views/photo.html'
+    })
+    .otherwise({'redirectTo': '/'});
 });
